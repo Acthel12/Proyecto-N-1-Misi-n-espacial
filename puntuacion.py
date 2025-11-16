9189d18827c580893f7559c7480775bd401dbf1b
Gabriel <gecgpro12@gmail.com>
2025-11-16 08:44:29 -0400

    añadida aclaración sobre el uso de recursos en la función de puntuación final

--- puntuacion.py
+++ puntuacion.py
@@ -3,4 +3,5 @@ import recursos
 def puntuacion_final():
     """Calcula y devuelve la puntuación final basada en los recursos restantes."""
     # Programa la lógica de puntuación aquí y borra este mensaje y pass
+    # Llamas a los recursos como recursos.oxigeno, recursos.combustible, etc. Puedes revizar recursos.py para ver los nombres exactos de las variables.
     pass
