d0f74c799867703d0d3545a64d5855fccca2b3d1
Gabriel <gecgpro12@gmail.com>
2025-11-17 20:18:08 -0400

    Añadidos unos eventos y balanceados los ya existentes para aumentar la dificultad general

--- eventos.py
+++ eventos.py
@@ -2,7 +2,7 @@ import random
 import recursos
 
 eventos_ocurridos = " "
-n = 12 # Número total de eventos disponibles Añadir más eventos y actualizar este número
+n = 13 #Número total de eventos disponibles Añadir más eventos y actualizar este número
 ## Traten de añadir eventos variados , que afecten diferentes recursos y que tengan varias opciones
 
 ## Selección aleatoria de eventos Nota: agregar más eventos a medida que se creen eventos y actualizar el valor de n
@@ -57,21 +57,26 @@ def evento_aleatorio():
     elif eventos == 12:
         refugio_cosmico()
         añadir_evento(eventos)
+    elif eventos == 13:
+        abandonar_tripulacion()
+        añadir_evento(eventos)
+    elif eventos == 14:
+        estacion_de_servicio()
+        añadir_evento(eventos)
     input("Presiona Enter para continuar...")
 
-## Los eventos por ahora solo afetan -5 o +5 a los recursos, pero se pueden modificar
 
 ## Eventos aleatorios que aumetan los recursos
 def asteriode_metalico():
     """Evento que otorga suministros y moral."""
     print("¡Has encontrado un asteroide metálico rico en recursos!")
-    recursos.actualizar_recurso("suministros", 5)
-    recursos.actualizar_recurso("moral", 5)
+    recursos.actualizar_recurso("suministros", 15)
+    recursos.actualizar_recurso("moral", 15)
 
 def salto_gravitacional():
     """Evento que reduce la distancia restante (avance gratuito)."""
     print("Un campo gravitacional te impulsa cientos de kilómetros hacia adelante.")
-    recursos.actualizar_recurso("distancia", 50)
+    recursos.actualizar_recurso("distancia", 100)
 
 def refugio_cosmico():
     """Evento que otorga moral y reduce días restantes (descanso eficiente)."""
@@ -81,9 +86,11 @@ def refugio_cosmico():
         print("Opción no válida. Intente de nuevo.")
         opcion = input("¿Deseas aprovechar el refugio para mejorar la moral y reducir un día de viaje? (s/n): ").lower()
     if opcion == 'n':
-        print("Decides no aprovechar el refugio.")
+        print("Decides no aprovechar el refugio. La tripulación se siente un poco desanimada.")
+        recursos.actualizar_recurso("moral", -25)
     else:
-        recursos.actualizar_recurso("moral", 8)
+        print("Aprovechas el refugio. La tripulación se siente renovada y descansada.")
+        recursos.actualizar_recurso("moral", 25)
         recursos.actualizar_recurso("dias_restantes", -1)
 
 #Eventos aleatorios que reducen los recursos
@@ -98,25 +105,25 @@ def tormenta_cosmica_():
         recurso = random.choice("cd")
         if recurso == "c":
             print("Has perdido algo de combustible al esquivar la tormenta.")
-            recursos.actualizar_recurso("combustible", -5)
+            recursos.actualizar_recurso("combustible", -20)
         else:
             print("Has perdido algunos días al esquivar la tormenta.")
-            recursos.actualizar_recurso("dias", 2)
+            recursos.actualizar_recurso("dias_restantes", -3)
     else:
         suerte = random.randint(1, 100)
         if suerte >= 50:
             print("Has logrado atravesar la tormenta sin daños mayores.")
         else:
-            print("La tormenta ha causado daños significativos a tu nave.")
-            recursos.actualizar_recurso("integridad", -5)
-            recursos.actualizar_recurso("suministros", -5)
+            print("La tormenta ha causado daños significativos a tu nave, y a afectado los sistemas electronicos de ella.")
+            recursos.actualizar_recurso("integridad", -40)
+            recursos.actualizar_recurso("energia", -35)
 
 ## Eventos de perdida de recursos
 def tormenta_cosmica_repentina():
     """Evento que reduce integridad y energia."""
     print("¡Una tormenta cósmica ha aparecido repentinamente y a dañado tu nave!")
-    recursos.actualizar_recurso("integridad", -5)
-    recursos.actualizar_recurso("energia", -5)
+    recursos.actualizar_recurso("integridad", -25)
+    recursos.actualizar_recurso("energia", -15)
 
 ## Eventos que sacrifican suministros para mejorar otros recursos
 def repartir_suministros():
@@ -130,30 +137,47 @@ def repartir_suministros():
             eleccion = input("¿Deseas aumentar la ración de suministros? (s/n): ").lower()
         if eleccion == 's':
             print("Has aumentado la ración de suministros, mejorando la moral de la tripulación.")
-            recursos.actualizar_recurso("suministros", -5)
-            recursos.actualizar_recurso("moral", 5)
+            recursos.actualizar_recurso("suministros", -40)
+            recursos.actualizar_recurso("moral", 40)
         else:
             print("Decides no aumentar la ración de suministros.")
             suerte = random.randint(1, 100)
             if suerte <= 50:
                 print("La moral de la tripulación ha disminuido debido a la falta de atención.")
-                recursos.actualizar_recurso("moral", -5)
+                recursos.actualizar_recurso("moral", -40)
             else:
                 print("La moral de la tripulación se mantiene estable.")
     else:
         print("La moral de la tripulación es alta, no es necesario aumentar la ración de suministros.")
 
+def abandonar_tripulacion():
+    #Idea de Sara, por abandonarla en la beca XD
+    """Evento donde abandonamos parte de la tripulación para ahorrar suministros."""
+    print("A habido un problema en la gestion de recursos y se ha decidido que parte de la tripulacion debe abandonar la nave para ahorrar suministros.")
+    eleccion = input("¿Deseas que parte de la tripulación abandone la nave? (s/n): ").lower()
+    while eleccion != 's' and eleccion != 'n':
+        print("Opción no válida. Intente de nuevo.")
+        eleccion = input("¿Deseas que parte de la tripulación abandone la nave? (s/n): ").lower()
+    if eleccion == 's':
+        print("Parte de la tripulación ha abandonado la nave, ahorrando suministros pero reduciendo la moral.")
+        recursos.actualizar_recurso("moral", -30)
+    else:
+        print("Decides no abandonar a la tripulación, La tripulacion se alegra de tu decision y confia mas en ti.")
+        print("Sin embargo, los suministros se han vuelto críticos debido a la sobrecarga de la tripulación.")
+        recursos.actualizar_recurso("moral", 10)
+        recursos.actualizar_recurso("suministros", -40)
+          
 ## Eventos para ganar combustible
 def encontrar_combustible():
     """Evento que otorga combustible."""
     print("¡Has encontrado un depósito de combustible flotando en el espacio!")
-    recursos.actualizar_recurso("combustible", 5)
+    recursos.actualizar_recurso("combustible", 25)
 
 def ganar_combustible_moral():
     """Evento que otorga combustible y moral."""
     print("¡Un miembro de la tripulación ha encontrado una manera de optimizar el uso del combustible!")
-    recursos.actualizar_recurso("combustible", 10)
-    recursos.actualizar_recurso("moral", 5)
+    recursos.actualizar_recurso("combustible", 20)
+    recursos.actualizar_recurso("moral", 20)
     
 def minar_combustible():
     """Evento que permite minar combustible a costa de suministros."""
@@ -164,28 +188,42 @@ def minar_combustible():
         eleccion = input("¿Deseas minar el combustible? (s/n): ").lower()
     if eleccion == "s":
         print("Has minado combustible, pero has consumido algunos suministros en el proceso.")
-        recursos.actualizar_recurso("combustible", 20)
-        recursos.actualizar_recurso("suministros", -5)
-        print(f"Tienes disponible {recursos.combustible}% de combustible y {recursos.suministros} de suministros.")
+        recursos.actualizar_recurso("combustible", 40)
+        recursos.actualizar_recurso("suministros", -25)
     else:
         print("Decides no minar el combustible.")
+
+def estacion_de_servicio():
+    """Evento que permite restablecer integridad a costa de unos días"""
+    print("Te has encontrado con una estacion de servicio aliada, mientras te dispones a recargar algo de combustible rapidamente")
+    print("la estacion conciente de tu mision desiden hacer una reparacion gratis, lo malo es que tardaran un par de dias")
+    eleccion = input("¿Acepta la reparación? (s/n): ").lower()
+    while eleccion != "s" and eleccion != "n":
+        print("Opción no válida. Intente de nuevo.")
+        eleccion = input("¿Acepta la reparación? (s/n): ").lower()
+    if eleccion == "s":
+        print("Has aceptado, ademas del combustible reparan tu casco.")
+        recursos.actualizar_recurso("combustible", 50)
+        recursos.actualizar_recurso("integridad", 50)
+        recursos.actualizar_recurso("dias_restantes", -4)
+    else:
+        print("Has rechazado amablemente la solicitud, no tienes tiempo que perder, te ponen una cantidad basica de combustible")
+        recursos.actualizar_recurso("combustible", 25)
 
 ## Eventos para ganar recursos
 def ganar_energia():
     """Evento que otorga energía."""
     print("¡Has encontrado una fuente de energía renovable en el espacio!")
-    recursos.actualizar_recurso("energia", 10)
-    print(f"Tienes disponible {recursos.energia}% de energía.")
+    recursos.actualizar_recurso("energia", 20)
 
 def ganar_oxigeno():
     """Evento que otorga oxígeno."""
     print("¡Has descubierto una reserva de oxígeno en un asteroide cercano!")
-    recursos.actualizar_recurso("oxigeno", 10)
-    print(f"Tienes disponible {recursos.oxigeno}% de oxígeno.")
+    recursos.actualizar_recurso("oxigeno", 20)
 
 def ganar_suministros():
     """Evento que otorga suministros."""
     print("¡Has encontrado un cargamento abandonado de suministros!")
-    recursos.actualizar_recurso("suministros", 10)
-    print(f"Tienes disponible {recursos.suministros}% de suministros.")
+    recursos.actualizar_recurso("suministros", 20)
+
 
